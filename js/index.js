375461e7a8dc15c9fe11e8f3be5bfa382eb0d25a
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-04-23 11:52:35 +0200

    images

--- js/index.js
+++ js/index.js
@@ -9,6 +9,20 @@
         const productContainer = document.createElement("div");
         productContainer.classList.add("product");
         productContainer.id = product.id;
+
+        const title = document.createElement("h2");
+        title.innerText = product.name;
+        productContainer.append(title);
+
+        for(let i = 0; i < product.images.length; i++) {
+            const imgData = product.images[i];
+            const img = document.createElement("img");
+            img.src = imgData.src;
+            img.alt = imgData.alt;
+            productContainer.append(img);
+        }
+        
+        container.append(productContainer);
     }
 
     function createProductsHTML(products) {
@@ -21,7 +35,7 @@
   
   async function myApp() {
     const products = await getProducts();
-    console.log(products);
+    createProductsHTML(products);
   }
 
 myApp();
