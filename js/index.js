9715f77ecb6e7cb0bd415b68391d9e24b77319e7
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-04-23 15:17:12 +0200

    featured

--- js/index.js
+++ js/index.js
@@ -1,6 +1,56 @@
 //Create JavaScript to fetch an array of product data from WordPress API
-  async function getProducts() {
+async function getProducts() {
     const response = await fetch("http://cmscarn.local/wp-json/wc/store/products");
+    return await response.json();
+  }
+  
+  async function myApp() {
+    const products = await getProducts();
+    console.log(products);
+  }
+myApp();
+
+
+
+//Create JavaScript functionality to fetch a single product’s data from WordPress API
+  async function getProductById(id) {
+    const response = await fetch(`http://cmscarn.local/wp-json/wc/store/products/${id}`);
+    return await response.json();
+  }
+  
+  function createProductHTML(product) {
+    const container = document.querySelector(".container2");
+          
+    const productContainer = document.createElement("div");
+    productContainer.classList.add("product");
+    productContainer.id = product.id;
+  
+    const title = document.createElement("h2");
+    title.innerText = product.name;
+    productContainer.append(title);
+  
+    for(let i = 0; i < product.images.length; i++) {
+      const imgData = product.images[i];
+      const img = document.createElement("img");
+      img.src = imgData.src;
+      img.alt = imgData.alt;
+      productContainer.append(img);
+    }
+          
+    container.append(productContainer);
+  }
+  
+  async function OneProductPage() {
+    const productId = 24;
+    const product = await getProductById(productId);
+    createProductHTML(product);
+  }
+  
+  OneProductPage();
+
+/// Create JavaScript functionality to render a grid of product data in HTML as thumbnails
+async function getProducts() {
+    const response = await fetch(`http://cmscarn.local/wp-json/wc/store/products`);
     return await response.json();
   }
     function createProductHTML(product) {
@@ -21,22 +71,21 @@
             img.alt = imgData.alt;
             productContainer.append(img);
         }
-        
         container.append(productContainer);
     }
 
     function createProductsHTML(products) {
     for (let i = 0; i < products.length; i++) {
         const product = products[i];
-        createProductHTML(product)
-
+        createProductHTML(product);
     }
   }
-  
-  async function myApp() {
+
+  async function productPage() {
     const products = await getProducts();
     createProductsHTML(products);
   }
 
-myApp();
+  productPage();
+///
 
